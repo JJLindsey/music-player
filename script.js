0d6d43eb7bdb1f1af48725843b2398ea466f9e0b
JJLindsey <jlindsey010@gmail.com>
2021-10-19 11:54:24 -0700

    scripts add play pause

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
 
 // song titles
-const songs = ['Blooming_Tree', 'Starry_Eyed']
+const songs = ['trees', 'moonrise'];
 
 //track songs
-let songIndex = 1
+let songIndex = 1;
 
 //load songs into DPOM
 loadSong(songs[songIndex])
 
 //update song details
 function loadSong(song) {
-    title.innerText = song
+
     audio.src = `music/${song}.wav`
-    cover.src = `images/${song}.jpg`
+    //cover.src = `images/${song}.jpg`
+    title.innerText = song
 }
 
 
@@ -42,11 +43,41 @@ function pauseSong() {
 }
 
 function prevSong() {
-    
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong();
 }
 
 function nextSong() {
+    songIndex++;
 
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong();
+}
+
+function updateProgess(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime - (clickX / width) * duration
 }
 
 // event listeners
@@ -64,3 +95,8 @@ playBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
 
+audio.addEventListener('timeupdate', updateProgess)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
