52d7a18160ae7e437ac2d420f4fba49bfa3e74ab
JJLindsey <jlindsey010@gmail.com>
2021-10-19 10:42:59 -0700

    add js functions

---
+++ script.js
@@ -0,0 +1,66 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+// song titles
+const songs = ['Blooming_Tree', 'Starry_Eyed']
+
+//track songs
+let songIndex = 1
+
+//load songs into DPOM
+loadSong(songs[songIndex])
+
+//update song details
+function loadSong(song) {
+    title.innerText = song
+    audio.src = `music/${song}.wav`
+    cover.src = `images/${song}.jpg`
+}
+
+
+function playSong() {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong() {
+    
+}
+
+function nextSong() {
+
+}
+
+// event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong()   
+    } else {
+        playSong()
+    }
+})
+
+//change song
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
